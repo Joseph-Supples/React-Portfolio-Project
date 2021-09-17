8c9740f87a3096b0a2035ca79769eac963849f9c
Joseph Supples <joseph.craig94@gmail.com>
2021-09-17 10:13:01 -0700

    React Portfolio Project - Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import Logo from "../jest_logo.png";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faBars} from "@fortawesome/free-solid-svg-icons";
+
+const Navbar = () => {
+    return (
+     <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+     <div className="container">
+        <a className="navbar-brand" href="#"><img src={Logo} alt="Logo" className="logo" ></img></a>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <FontAwesomeIcon icon={faBars} style = {{color: "#fff"}}/>
+     </button>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav ms-auto">
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Home</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">About me</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Services</a>
+      </li><li className="nav-item">
+        <a className="nav-link" href="#">Portfolio</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Contact Me</a>
+      </li>
+      </ul>
+  </div>
+</div> 
+</nav>
+    )
+}
+
+export default Navbar
